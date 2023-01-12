b64950ae790b802778f8b69946db113c9e68cd28
Okellothomas <thomasolal33@gmail.com>
2023-01-12 13:06:44 +0300

    save changes

---
+++ escapeCharacter.js
@@ -0,0 +1,23 @@
+/**
+ * The following escape characters are crucial for string laterals
+ * \' single quote
+ * \" double quote
+ * \\ backlash
+ * \n newline
+ * \r carriage return
+ * \t tab
+ * \b backspace
+ * \f form feed 
+ */
+
+var myString = "Okello is my Name\n\t\\ and that should not be any issue sure? \n\t\r Thanks you for listening \n\b"
+var escapeOne = `Okello sure "we should go home now!" and 'tommorrow we start working on something brand new!'`
+
+console.log(myString)
+console.log(escapeOne)
+
+/***
+ * String concatination using the plus operator
+ */
+
+stringOne = "Sure kanga is my school!"
---
+++ index.js
@@ -0,0 +1,55 @@
+/*
+Variable declaration, initialization and assignment. 
+*/
+
+var name;
+var school;
+var age;
+var div;
+
+// variable initialization 
+name = "okello"
+school = "Kanga"
+age = 26
+div = 0
+
+//variable assigment
+
+name = name + "Thomas"
+school = school + "School"
+age = age + age
+divNumbers = 66 / div
+
+// output the variables values
+
+alert(name)
+console.log(school)
+alert(age)
+console.log(divNumbers)
+
+
+// incrementing and decrementing in javascript
+
+var increase = 11
+var decrease = 11
+
+
+// implementing the code, the same implementation can apply for all the four mathematical operations. 
+
+increase -= 8
+decrease += 8
+
+
+// first output
+console.log(increase)
+console.log(decrease)
+
+increase--
+decrease++
+
+// second output
+
+console.log(increase)
+console.log(decrease)
+
+// 
