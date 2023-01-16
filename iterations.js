052f6a1dc9a758c67cc00b26bba93ee69933c2f2
Okellothomas <thomasolal33@gmail.com>
2023-01-16 05:38:59 +0300

    add the while loop implementation

---
+++ iterations.js
@@ -0,0 +1,28 @@
+/**
+ * We will be writing different iterations starting from the while loops. 
+ */
+
+var myArray = []
+
+var i = 1;
+
+while (i < 20) {
+    myArray.push(i)
+    i++
+}
+
+console.log(myArray)
+
+/**
+ * Write a while loop to print out i love you 100 times
+ */
+
+var y = 1;
+
+while (y < 101) {
+    var p = "I love you "
+    y++
+}
+// not worked as expected.
+console.log(p)
+
