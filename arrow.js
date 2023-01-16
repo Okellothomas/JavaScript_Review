0af8d94847a66357e42cff5529089a3a1b4329d5
Okellothomas <thomasolal33@gmail.com>
2023-01-16 12:29:45 +0300

    exit arrow

--- arrow.js
+++ arrow.js
@@ -15,3 +15,28 @@ console.log(Arrow)
 const Arrowy = () => new Date()
 
 console.log(Arrowy)
+
+var myConcat = function (arr1, arr3) {
+    return arr1.concat(arr3)
+}
+
+console.log(myConcat([1, 2], [3, 4]))
+
+
+// converting to an arrow function 
+
+const myConcut = (arrOne, arrTwo) => arrOne.concat(arrTwo) 
+
+console.log(myConcut([2, 3], [1, 2, 3, 4]))
+
+// More on arrow function 
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, 2]
+
+const squarList = (arr) => {
+    const squareIntegers = arr.filter(num => Number.isInteger(num) && num > 0).map(x => x * x)
+    return squareIntegers
+}
+
+const squareIntegers = squarList(realNumberArray)
+console.log(squareIntegers)
