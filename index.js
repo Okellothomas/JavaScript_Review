b731bf9441adddbc72d06f36f7dae469cdbf6963
Okellothomas <thomasolal33@gmail.com>
2024-02-13 13:56:42 +0300

    add changes

--- index.js
+++ index.js
@@ -1,34 +1,131 @@
-// string is javascript
+/**
+ * The number datatype is one of the main datatype is javascript
+ */
 
-let myName = "Mathematics"
-// Checking the length of a string: returns length of string
-console.log(myName.length)
-console.log('Jkuat is a good university'.length)
-// CharAt() :  returns the character at the position we are
-console.log(myName.charAt(8))
-// indexOf provide the first occurance in teh string
-console.log(myName.indexOf('mat'))
-// lastIndexOf('qw'): provide the last occurance of the string or group character
-console.log(myName.lastIndexOf('mat'))
-// slice (3, 5) : return the characters from the starting position to the ending position:
-console.log(myName.slice(2, 6))
-console.log(myName.slice(5)) // return from index 5 and onwards
-console.log(myName.slice(9, 0)) // returns nothing
-console.log(myName.slice(5, 6)) // returns only one character
-// toUpperCase() : convert the string to uppercase
-console.log(myName.toLocaleUpperCase())
-// tolowerCase(): conver the string to lowercase
-console.log(myName.toLocaleLowerCase())
-// iclude:  returns boolean data
-console.log(myName.includes('ath'))
-// split('e') slipt the string where the charact occurs and returns an array
-console.log(myName.split('e')) // excludes 'e'
-console.log(myName.split('')) //  returns every character as a separte string 
-console.log('john, joe, dave'.split(',')); // returns the strings separated array members
-console.log('Every good boy does fine'.split(' ')) // returns each letter as string in the array
+const myNumber = 24;
 
-/* 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-The link extensively captures the all javascript essentials
+const myFloat = 24.099329;
 
-*/
+const myString = '59'
+
+console.log(myNumber === myFloat)
+
+// using toStringfy and parseInt
+
+
+console.log(Number(myString))
+
+console.log(myFloat.toString() * 3)
+
+// numbers method 
+
+// The number.isInteger() method determines wheather the passed value is an interger
+
+// The Number.parseFloat() method parses an argument and returns a flaoting point number. If a number cannot be parsed from the argument, it returns NaN. 
+
+console.log(Number.parseFloat(myNumber))
+
+console.log(typeof Number.parseFloat(myString))
+
+console.log(parseFloat(myNumber))
+console.log(myFloat.toFixed(1))
+
+// Chaining 
+
+console.log(Number.parseFloat("4.2939292").toFixed(2).toString().toLowerCase())
+
+console.log(Number.isNaN('Dave'))
+
+
+/**
+ * Static methods
+Number.isFinite()
+Determine whether the passed value is a finite number.
+
+Number.isInteger()
+Determine whether the passed value is an integer.
+
+Number.isNaN()
+Determine whether the passed value is NaN.
+
+Number.isSafeInteger()
+Determine whether the passed value is a safe integer (number between -(253 - 1) and 253 - 1).
+
+Number.parseFloat()
+This is the same as the global parseFloat() function.
+
+Number.parseInt()
+This is the same as the global parseInt() function.
+Instance methods
+Number.prototype.toExponential()
+Returns a string representing the number in exponential notation.
+
+Number.prototype.toFixed()
+Returns a string representing the number in fixed-point notation.
+
+Number.prototype.toLocaleString()
+Returns a string with a language sensitive representation of this number. Overrides the Object.prototype.toLocaleString() method.
+
+Number.prototype.toPrecision()
+Returns a string representing the number to a specified precision in fixed-point or exponential notation.
+
+Number.prototype.toString()
+Returns a string representing the specified object in the specified radix ("base"). Overrides the Object.prototype.toString() method.
+
+Number.prototype.valueOf()
+Returns the primitive value of the specified object. Overrides the Object.prototype.valueOf() method.
+ */
+
+const biggestNum = Number.MAX_VALUE;
+const smallestNum = Number.MIN_VALUE;
+const infiniteNum = Number.POSITIVE_INFINITY;
+const negInfiniteNum = Number.NEGATIVE_INFINITY;
+const notANum = Number.NaN;
+
+console.log('***************************************')
+console.log(biggestNum)
+console.log(smallestNum)
+console.log(infiniteNum)
+console.log(negInfiniteNum)
+console.log(notANum)
+
+
+/**
+ * Integer range for Number
+The following example shows the minimum and maximum integer values that can be represented as Number object.
+ */
+
+const biggestInt = Number.MAX_SAFE_INTEGER; // (2**53 - 1) => 9007199254740991
+const smallestInt = Number.MIN_SAFE_INTEGER; // -(2**53 - 1) => -9007199254740991
+
+console.log(biggestInt)
+console.log(smallestInt)
+
+
+/**
+ * Using Number() to convert a Date object
+The following example converts the Date object to a numerical value using Number as a function:
+ */
+
+const d = new Date("1995-12-17T03:24:00");
+console.log(Number(d));
+
+/**
+ * Convert numeric strings and null to numbers
+ */
+
+console.log('*******************************')
+console.log()
+console.log(Number("123")) // 123
+console.log(Number("123") === 123) //true
+console.log(Number("12.3")) // 12.3
+console.log(Number("12.00")) // 12.3
+console.log(Number("123e-1")) // 12.3
+console.log(Number(""))
+console.log(Number(null))
+console.log(Number("0x11"))
+console.log(Number("0b11"))
+console.log(Number("0o11"))
+console.log(Number("foo"))
+console.log(Number("100a"))
+console.log(Number("-Infinity"))
